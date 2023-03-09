3dd0ebba8284e339ef9f6e8dffeef6c76f85c943
hatn <to7@foxmail.com>
2023-03-10 03:34:53 +0800

    v1.2.1
    最大6页： 480条记录

--- dmhyorg.py
+++ dmhyorg.py
@@ -36,29 +36,21 @@ except ModuleNotFoundError:
 class dmhyorg(object):
 	url = "https://share.dmhy.org"
 	name = "DMHY"
+	page_max = 6 # total: 80 * page_max
 	supported_categories = {"all": 0, "anime": 2, "pictures": 3, "music": 4, "tv": 6, "games": 9}
 	
 	table_reg = r'<table[^>]*id="topic_list"[^>]*>\s*<thead>[\s\S]+?</thead>\s*<tbody>([\s\S]+?)</tbody>\s*</table>'
-	#table_reg = r'<table([^>]*)id="topic_list"([^>]*)>' #
 	tr_reg = r'(<tr[^>]*>[\s\S]+?</tr>)'
 	reg = r'<tr\s+class="[^"]*">\s+<td\s+width="[^"]+">[\s\S]+?</td>\s+<td\s+width="[^"]+"\s+align="center">[\s\S]+?</td>\s+<td\s+class="title">[\s\S]*?<a\s+href="([^"]+)"\s+target="_blank"\s*>\s*([\s\S]+?)\s*?</a>[\s\S]+?</td>[\s\S]+?<td\s+nowrap="nowrap"\s+align="center">\s+<a\s+class="download-arrow arrow-magnet"\s+title="磁力下載"\s+href="([^"]+)">[^<]+</a>[\s\S]+?</td>\s+<td\s+nowrap="nowrap"\s+align="center">([\s\S]+?)</td>[^<]+<td\s+nowrap="nowrap"\s+align="center"><span class="btl_1">([\s\S]+?)</span></td>[^<]+<td\s+nowrap="nowrap"\s+align="center"><span\s+class="bts_1">([\s\S]+?)</span></td>[^<]+<td\s+nowrap="nowrap"\s+align="center">([\s\S]+?)</td>\s+<td\s+align="center"><a\s+href="[^"]+">[\s\S]+?</a></td>\s+</tr>'
 
 	def get_data(self, url):
 		html = retrieve_url(url)
 		result = re.findall(self.table_reg, html)
-		"""if not result:
-		print(result)
-		exit()
-		"""
 		html_raw = result[0]
 		tr_raw = re.findall(self.tr_reg, html_raw)
-		#print(tr_raw[0])
-		#exit()
 		data, item, name = [], {}, ''
 		for tr in tr_raw:
 			result = re.findall(self.reg, tr)
-			#print(result)
-			#exit()
 			for v in result:
 				name = re.compile(r'<[^>]+>', re.S).sub('', v[1]).replace('\t', '').replace('\n', '').replace('\xa0', '')
 				item = {'link': v[2], 'name': name, 'desc_link': self.url + v[0], 'size': v[3], 'seeds': v[4], 'leech': v[5], 'engine_url': self.url}
@@ -80,7 +72,7 @@ class dmhyorg(object):
 			if page >= 2 or len < 80:
 				break
 			page += 1
-		#return total
+		return total
 
 """ test
 dmhy = dmhyorg()
